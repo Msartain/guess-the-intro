cd3f1046d14a8f1bda931277cc05acb1d13b0ce8
Matthew Sartain <matthewsartain@me.com>
2019-11-01 15:48:31 -0500

    song play for 5 seconds

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 // - - - - - - - const Variables - - - - - - -
 
 const songs = {
-    oneHand: "assests/songs/Alanis_Morissette-Hand_In_My_Pocket.mp3",
-    lovelyDay: "assests/songs/Bill_Withers-Lovely_Day.mp3",
-    smallThings: "assests/songs/Blink-182-All_The_Small_Things.mp3",
-    sumOf69: "assests/songs/Bryan_Adams-Summer_Of_'69.mp3",
-    yellow: "assests/songs/Coldplay-Yellow.mp3",
-    wantYouBack: "assests/songs/Jackson_5-I_Want_You_Back.mp3",
-    allMyFriends: "assests/songs/LCD_Soundsystem-All_my_friends.mp3",
-    leveeBreaks: "assests/songs/Led_Zeppelin-When_the_Levee_Breaks.mp3",
-    champSuper: "assests/songs/Oasis-Champagne_Supernova.mp3",
-    inTheAir: "assests/songs/Phil_Collins-In_the_Air_Tonight.mp3",
-    fatBottom: "assests/songs/Queen-Fat_Bottomed_Girls.mp3",
-    wannabe: "assests/songs/Spice_Girls-Wannabe.mp3"
-}
-
-
-
-
+     oneHand: "assests/songs/Alanis_Morissette-Hand_In_My_Pocket.mp3",
+     lovelyDay: "assests/songs/Bill_Withers-Lovely_Day.mp3" ,
+     smallThings: "assests/songs/Blink-182-All_The_Small_Things.mp3" ,
+     sumOf69: "assests/songs/Bryan_Adams-Summer_Of_'69.mp3" ,
+     yellow: "assests/songs/Coldplay-Yellow.mp3" ,
+     wantYouBack: "assests/songs/Jackson_5-I_Want_You_Back.mp3" ,
+     allMyFriends: "assests/songs/LCD_Soundsystem-All_my_friends.mp3" ,
+     leveeBreaks: "assests/songs/Led_Zeppelin-When_the_Levee_Breaks.mp3" ,
+     champSuper: "assests/songs/Oasis-Champagne_Supernova.mp3" ,
+     inTheAir: "assests/songs/Phil_Collins-In_the_Air_Tonight.mp3" ,
+     fatBottom: "assests/songs/Queen-Fat_Bottomed_Girls.mp3" ,
+     wannabe: "assests/songs/Spice_Girls-Wannabe.mp3" 
+};
+const player = new Audio();
 
 // - - - - - - - State Variables - - - - - - -
 
+let score = 0;
 
-
+let usedSongs = [];
 
 
 // - - - - - - - Cached Elements - - - - - - -
@@ -33,8 +31,27 @@ const songs = {
 
 // - - - - - - - Event Listeners - - - - - - -
 
-
+document.getElementById('play-button').addEventListener('click', playSong)
 
 
 
 // - - - - - - - - Functions - - - - - - - - -
+
+
+
+function playSong(){
+    let song = songs.oneHand;
+    player.src = song;
+    player.currentTime = 0; 
+    player.play(song); 
+    setInterval(function(){
+        if(player.currentTime > 5){
+            player.pause();
+        }
+    })
+};
+
+// function randomSong(obj){
+//     var keys = songs.keys(obj)
+//     return obj[keys[keys.length * Math.random() << 0 ]]
+// }
