2346d0c87dfc6e1422bcde4aa20c702734332044
Matthew Sartain <matthewsartain@me.com>
2019-11-01 13:20:09 -0500

    created const with songs

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// - - - - - - - const Variables - - - - - - -
+
+const songs = {
+    oneHand: "assests/songs/Alanis_Morissette-Hand_In_My_Pocket.mp3",
+    lovelyDay: "assests/songs/Bill_Withers-Lovely_Day.mp3",
+    smallThings: "assests/songs/Blink-182-All_The_Small_Things.mp3",
+    sumOf69: "assests/songs/Bryan_Adams-Summer_Of_'69.mp3",
+    yellow: "assests/songs/Coldplay-Yellow.mp3",
+    wantYouBack: "assests/songs/Jackson_5-I_Want_You_Back.mp3",
+    allMyFriends: "assests/songs/LCD_Soundsystem-All_my_friends.mp3",
+    leveeBreaks: "assests/songs/Led_Zeppelin-When_the_Levee_Breaks.mp3",
+    champSuper: "assests/songs/Oasis-Champagne_Supernova.mp3",
+    inTheAir: "assests/songs/Phil_Collins-In_the_Air_Tonight.mp3",
+    fatBottom: "assests/songs/Queen-Fat_Bottomed_Girls.mp3",
+    wannabe: "assests/songs/Spice_Girls-Wannabe.mp3"
+}
+
+
+
+
+
+// - - - - - - - State Variables - - - - - - -
+
+
+
+
+
+// - - - - - - - Cached Elements - - - - - - -
+
+
+
+
+
+// - - - - - - - Event Listeners - - - - - - -
+
+
+
+
+
+// - - - - - - - - Functions - - - - - - - - -
