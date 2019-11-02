27c51fe22e8020e07153bd4fa292873a62fe492b
Matthew Sartain <matthewsartain@me.com>
2019-11-02 13:41:32 -0500

    randomizes songs and wont repeat a song

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 // - - - - - - - const Variables - - - - - - -
-// step 1 - organize data types
-// paths array, arist array, choices array of arrays 
-//step 2 make a function that ramdomizes the song and arrays + matches up with artist arr and choies arr
+// matches up with artist arr and choies arr
 const songs = [
-     { oneHand: "assests/songs/Alanis_Morissette-Hand_In_My_Pocket.mp3"},
-     { lovelyDay: "assests/songs/Bill_Withers-Lovely_Day.mp3" },
-     { smallThings: "assests/songs/Blink-182-All_The_Small_Things.mp3" },
-     { sumOf69: "assests/songs/Bryan_Adams-Summer_Of_'69.mp3" },
-     { yellow: "assests/songs/Coldplay-Yellow.mp3" },
-     { wantYouBack: "assests/songs/Jackson_5-I_Want_You_Back.mp3" },
-     { leveeBreaks: "assests/songs/Led_Zeppelin-When_the_Levee_Breaks.mp3" },
-     { champSuper: "assests/songs/Oasis-Champagne_Supernova.mp3" },
-     { inTheAir: "assests/songs/Phil_Collins-In_the_Air_Tonight.mp3" },
-     { fatBottom: "assests/songs/Queen-Fat_Bottomed_Girls.mp3" },
-     { wannabe: "assests/songs/Spice_Girls-Wannabe.mp3" } 
+     "assests/songs/Alanis_Morissette-Hand_In_My_Pocket.mp3",
+     "assests/songs/Bill_Withers-Lovely_Day.mp3",
+     "assests/songs/Blink-182-All_The_Small_Things.mp3",
+     "assests/songs/Bryan_Adams-Summer_Of_'69.mp3",
+     "assests/songs/Coldplay-Yellow.mp3",
+     "assests/songs/Jackson_5-I_Want_You_Back.mp3",
+     "assests/songs/Led_Zeppelin-When_the_Levee_Breaks.mp3",
+     "assests/songs/Oasis-Champagne_Supernova.mp3",
+     "assests/songs/Phil_Collins-In_the_Air_Tonight.mp3",
+     "assests/songs/Queen-Fat_Bottomed_Girls.mp3",
+     "assests/songs/Spice_Girls-Wannabe.mp3" 
 ];
 
-const artists = ['Alanis Morissette',
-                 'Bill Withers', 
-                 'Blink 182', 
-                 'Bryan Adams', 
-                 'Coldplay', 
-                 'Jackson 5',  
-                 'Led Zeppellin', 
-                 'Oasis', 
-                 'Phil Collins', 
-                 'Queen', 
-                 'The Spice Girls'
-                ];
+const correctGuesses = [
+    'Alanis Morissette',
+    'Bill Withers', 
+    'Blink 182', 
+    'Bryan Adams', 
+    'Coldplay', 
+    'Jackson 5',  
+    'Led Zeppellin', 
+    'Oasis', 
+    'Phil Collins', 
+    'Queen', 
+    'The Spice Girls'
+];
 
-const guesses = [
+const wrongGuesses = [
     ['Madonna', 'Avril Lavigne', 'Taylor Swift'],
     ['Lionel Richie', 'Marvin Gaye', 'Nina Simone'],
     ['Sum 41', 'Green Day', 'Good Charlotte'],
@@ -78,11 +77,14 @@ function playSong(){
             player.pause();
         }
     })
+    console.log(usedSongs)
 };
 
 function randomizeSongs(arr){
-    // let randomSong = arr[Math.floor(Math.random() * songs.length)];
-    let randomSong = arr[0].oneHand;
+    let randomSong = arr[Math.floor(Math.random() * songs.length)];
+    let i = arr.indexOf(randomSong);
+    arr.splice(i, 1);
+    usedSongs.push(i)
     return randomSong;
 };
 
